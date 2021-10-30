5a17c4060807e85f09a6fe15e42091b46203f975
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-10-30 11:57:41 +0100

    transaction view

--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from api.serializers import RegisterUserSerializer
+from transaction.models import Transaction
+
 
 User = get_user_model()
 
@@ -21,6 +25,11 @@ class RegisterUserView(CreateAPIView):
         return Response(response.data, status=status.HTTP_201_CREATED)
 
 
-class PaymentView(APIView):
-    pass
+class TransactionList(ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Transaction.objects.filter(user=user)
+        return queryset
