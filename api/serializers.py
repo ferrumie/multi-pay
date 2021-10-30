00adc8b70122bdc40bf7832e245aad599f43a730
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-10-30 10:08:45 +0100

    retrieve token

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,48 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.contrib.auth import password_validation
 
 User = get_user_model()
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
     """Serializer for user registeration"""
+    password = serializers.CharField(
+        max_length=255,
+        required=True,
+        style={"input_type": "password"},
+        write_only=True
+    )
+    password2 = serializers.CharField(
+        max_length=255,
+        required=True,
+        style={"input_type": "password"},
+        write_only=True
+    )
+
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email',
-                  'phone_number']
+                  'phone_number', 'password', 'password2']
 
     def validate(self, attrs):
         if User.objects.filter(email=attrs["email"]).exists():
             raise serializers.ValidationError(
                 "A user with this email allready exists")
+        if attrs["password"] != attrs.pop("password2"):
+            raise serializers.ValidationError("Passwords do not match")
+
+        try:
+            password_validation.validate_password(attrs["password"])
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+
         return attrs
+
+    def create(self, validated_data):
+        password = validated_data.get('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from api.views import RegisterUserView
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     # Register
     path('user/register/', RegisterUserView.as_view(), name="register-user"),
+    path('user/view-token/', obtain_auth_token, name='token-view')
 
 
 ]
