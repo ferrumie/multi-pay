20ea2bb8471ede7b4e384c5b0ac70066170b7a20
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-10-30 11:45:30 +0100

    transaction model

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+    amount = models.DecimalField(
+        _("Plan Amount"), decimal_places=2, max_digits=12,
+        blank=True, null=True)
+
+    api_key = models.CharField(
+        _('Verified'), max_length=255, unique=True)
+
+    platform = models.CharField(
+        _('Payment Platform'), max_length=150)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
