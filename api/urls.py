eb39078881f62ab7ae45bc522374d1cbf936d590
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-10-30 10:09:08 +0100

    update url

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from api.authentication import CustomAuthToken
 
 from api.views import RegisterUserView
 from rest_framework.authtoken.views import obtain_auth_token
@@ -7,7 +8,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     # Register
     path('user/register/', RegisterUserView.as_view(), name="register-user"),
-    path('user/view-token/', obtain_auth_token, name='token-view')
+    path('user/view-token/', CustomAuthToken.as_view(), name='token-view')
 
 
 ]
