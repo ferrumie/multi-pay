8c98b208e4413c8c0011dda9323f2900cb5b59da
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-11-04 08:17:09 +0100

    create base test

---
+++ api/tests/unit/base_test.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase, APIClient
+
+
+User = get_user_model()
+
+
+class BaseAPITestCase(APITestCase):
+
+    """
+    base setup to create different test cases for cleaner code
+    """
+    client = APIClient
+
+    def setUp(self):
+
+        user = User.objects.create_user(
+            email="nobody@nobody.niks", password="nobody"
+        )
+        user.is_staff = False
+        user.is_superuser = False
+        user.role = 'USER'
+        user.save()
+
+        user = User.objects.create_user(
+            email="somebody@nobody.niks", password="somebody"
+        )
+        user.is_staff = False
+        user.is_superuser = False
+        user.save()
+
+    def tearDown(self):
+        User.objects.filter(email="nobody@nobody.niks").delete()
+        User.objects.filter(email="somebody@nobody.niks").delete()
--- api/tests/unit/testuser.py
+++ api/tests/unit/testuser.py
@@ -45,16 +45,39 @@ class UserTestCase(APITestCase):
         self.assertIn('token', response.data)
 
     def test_validate_phone_number(self):
-        pass
+        ''' Create user with invalid phone number (following google phonelib format)'''
+        response = self.client.post(
+            reverse('register-user'), self.invalid_number_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('The phone number entered is not valid.',
+                      response.data['phone_number'])
 
     def test_validate_password(self):
-        pass
+        ''' Create user with unmatching password number'''
+        response = self.client.post(
+            reverse('register-user'), self.invalid_password_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('Passwords do not match',
+                      str(response.data))
 
     def test_already_existing_mail(self):
-        pass
+        ''' You cant create different users with a single email'''
+        response = self.client.post(
+            reverse('register-user'), self.register_user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Create another user with same email
+        response = self.client.post(
+            reverse('register-user'), self.register_user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn(
+            'user with this Email Address already exists.', response.data['email'])
 
     def test_add_api_key_for_payment_platform(self):
-        pass
+        self.client.login(email='nobody@nobody.niks', password='nobody')
+        # Create user
+        response = self.client.post(reverse('user_list'), self.create_user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_non_unique_api_key(self):
         pass
