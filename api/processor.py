8de14565351252ad2727f63b8cf5e9d86769228c
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-11-04 10:46:40 +0100

    paymen processor

--- api/processor.py
+++ api/processor.py
@@ -13,14 +13,23 @@ payment_methods.register_payment_method('stripe', StripePayment)
 
 
 class PaymentProcessor:
+    '''
+    This processes the method gotten from the payment method factory
+    It is called by the views,
+    Gets the payment method, (say, paystack) from payment method factory
+    then get the class responsible to the payment processing of that method
+    and calls either the pay or verify method
+    '''
     @classmethod
     def pay(cls, method, **kwargs):
+        ''' Calls the pay method of the payment class'''
         payment_method = payment_methods.select_payment_method(method)
         payload = kwargs
         return payment_method().pay(payload)
 
     @classmethod
     def verify(cls, method, **kwargs):
+        ''' Calls the verify method f the payment class'''
         payment_method = payment_methods.select_payment_method(method)
         kwargs['method'] = method
         payload = kwargs
